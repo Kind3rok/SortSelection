c317e7c0f6f9d75139896b7eddbd64fb71de9861
Kind3rok <108827009+Kind3rok@users.noreply.github.com>
2023-12-24 21:34:30 +0300

    добавление GUI

---
+++ RPS-3-Baev-Ivlev-425/selection_sort.cpp
@@ -0,0 +1,23 @@
+﻿#include "selection_sort.h"
+
+
+void SelectionSort::sort(std::vector<int> &vect) {
+    size_t n = vect.size();
+    size_t min_index = 0;
+
+    for (size_t i = 0; i < n - 1; i++) {
+        min_index = i;
+
+        for (size_t j = i + 1; j < n; j++) {
+            if (vect[j] < vect[min_index]) {
+                min_index = j;
+            }
+        }
+
+        if (min_index != i) {
+            int temp = vect[i];
+            vect[i] = vect[min_index];
+            vect[min_index] = temp;
+        }
+    }
+}
---
+++ RPS-3-Baev-Ivlev-425/selection_sort.h
@@ -0,0 +1,20 @@
+﻿#ifndef SELECTION_SORT_H
+#define SELECTION_SORT_H
+
+#include <vector>
+
+
+/// <summary>
+/// Класс, реализующий сортировку выбором.
+/// </summary>
+class SelectionSort {
+public:
+    /// <summary>
+    /// Сортирует вектор с использованием сортировки выбором.
+    /// </summary>
+    /// <param name="array">Вектор, который нужно отсортировать.</param>
+    void sort(std::vector<int> &array);
+};
+
+
+#endif
